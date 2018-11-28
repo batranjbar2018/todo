f84a4f3a2413b8f03cdd068caad2990406381ce2
Your Namebatool <batranjbar2018>
2018-11-28 14:44:31 +0330

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import store from './store';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       this.time = new Date()
+      </div>
+    );
+  }
+}
+
+export default App;
